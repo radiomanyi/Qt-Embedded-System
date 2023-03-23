46b91c9f0e25ecf66b877b3f61d307953a39706e
radiomanyi <pyzyf1983@gmail.com>
2023-03-23 22:13:27 +0800

    Demo Update

---
+++ Demo/BarControl/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.resize(400,300);
+    w.setWindowTitle("My Window");
+    w.show();
+
+    return a.exec();
+}
---
+++ Demo/MyCalculator/mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->lcdNumber->setPalette(Qt::green);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_Calculate_clicked()
+{
+    double num1 = ui->lineEditNum1->text().toDouble();//获取num1的值
+    double num2 = ui->lineEditNum2->text().toDouble();//获取num2的值
+  uint8_t index = ui->comboBox->currentIndex();       //获取下拉框的下标0 +, 1 - ,2 *, 3 /
+    double result = 0.0;
+
+        switch (index) {
+        case 0:
+            result = num1 + num2;
+            break;
+        case 1:
+            result = num1 - num2;
+            break;
+        case 2:
+            result = num1 * num2;
+            break;
+        case 3:
+            if(num2 == 0)
+            {
+                qDebug("num2 is 0");
+                QMessageBox::about(this,"注意","除数不能为0");
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            break;
+
+    }
+        ui->lineEditResult->setText(QString::number(result));
+        ui->lcdNumber->display(result);
+
+
+
+}
+
+void MainWindow::on_Clear_clicked()
+{
+    ui->lineEditNum1->clear();
+    ui->lineEditNum2->clear();
+    ui->lineEditResult->clear();
+}
